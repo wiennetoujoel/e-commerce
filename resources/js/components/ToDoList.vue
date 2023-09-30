ca6e520d733552f03db3442071f748aa3d56f238
wiennetoujoel <wiennetouj@gmail.com>
2023-09-30 13:40:51 +0700

    file lengkap

---
+++ resources/js/components/ToDoList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <h1>To Do</h1>
+        <input-form @emit-submit="addList"/>
+        <ul class="list">
+            <li v-for="(item, index) in list" :key="index">
+                {{ item.title }}
+                <span class="delete" @click="deleteList(index)">Hapus</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            list: [
+                { title: "menyapu" },
+                { title: "cuci piring" },
+                
+            ],
+            input: ""
+        }
+    },
+
+    methods: {
+        addList() {
+            let newList = {
+                title: datainput,
+            }
+            this.list.push(newList)
+        },
+        deleteList(index) {
+            this.list.splice(index, 1)
+        }
+    },
+    mounted() {
+        console.log("Component Mounted")
+    }
+}
+</script>
